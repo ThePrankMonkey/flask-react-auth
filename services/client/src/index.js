48374daf82f0af31d468921b61a90d54d239b17e
Matthew Hellmer <theprankmonkey@gmail.com>
2022-07-13 12:55:33 -0400

    Added About and moved to react-routes.

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -1,87 +1,14 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
-import React, { Component } from "react";
-import axios from "axios";
+import { BrowserRouter as Router } from "react-router-dom";
 
-import AddUser from "./components/AddUser";
-import UsersList from "./components/UsersList";
-
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      users: [],
-      username: "",
-      email: "",
-    };
-    this.addUser = this.addUser.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-  }
-  componentDidMount() {
-    this.getUsers();
-  }
-  addUser(event) {
-    event.preventDefault();
-    console.log("sanity check, addUser!");
-    const data = {
-      username: this.state.username,
-      email: this.state.email,
-    };
-    axios
-      .post(`${process.env.REACT_APP_API_SERVICE_URL}/users`, data)
-      .then((res) => {
-        console.log(res);
-        this.getUsers();
-        this.setState({ username: "", email: "" });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
-  getUsers() {
-    console.log("sanity check, getUsers!");
-    axios
-      .get(`${process.env.REACT_APP_API_SERVICE_URL}/users`)
-      .then((res) => {
-        this.setState({ users: res.data });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
-  handleChange(event) {
-    const obj = {};
-    obj[event.target.name] = event.target.value;
-    this.setState(obj);
-  }
-  render() {
-    return (
-      <section className="section">
-        <div className="container">
-          <div className="columns">
-            <div className="column is-one-third">
-              <br />
-              <h1 className="title is-1">Users</h1>
-              <hr />
-              <br />
-              <AddUser
-                username={this.state.username}
-                email={this.state.email}
-                addUser={this.addUser}
-                // eslint-disable-next-line react/jsx-handler-names
-                handleChange={this.handleChange}
-              />
-              <br />
-              <br />
-              <UsersList users={this.state.users} />
-            </div>
-          </div>
-        </div>
-      </section>
-    );
-  }
-}
+import App from "./App.jsx";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 
-root.render(<App />);
+root.render(
+  <Router>
+    <App />
+  </Router>
+);
